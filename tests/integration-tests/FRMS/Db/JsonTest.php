540bea0d447ae09bb63983fad01d86ac1d0472e0
unknown <Kyle.Almas@DFRFRM025686.de-ent.com>
2013-12-05 12:03:18 -0500

    implements Json class

--- tests/integration-tests/FRMS/Db/JsonTest.php
+++ tests/integration-tests/FRMS/Db/JsonTest.php
@@ -8,9 +8,47 @@ class JsonTest extends PHPUnit_Framework_TestCase {
 		$this->db = new FRMS\Db\Json($filepath);
 	}
 	
-	public function testFetch() {
+	public function test_fetch() {
 		$value = $this->db->fetch('alabama/montgomery');
 		$this->assertEquals('AL/Greater-Montgomery.php', $value);
+	}
+	
+	public function test_fetch_nonexistant_key_returns_null() {
+		$value = $this->db->fetch('virginia/norfork');
+		$this->assertEquals(null, $value);
+	}
+	
+	public function test_fetch_numeric_key_returns_null() {
+		$value = $this->db->fetch(9);
+		$this->assertEquals(null, $value);
+	}
+	
+	public function test_nonexistant_file_throws_exception_on_fetch(){
+		$filepath = __DIR__ . '/no_file_here.json';
+		$this->db = new FRMS\Db\Json($filepath);
+		
+		$caught = false;
+		try {
+			$value = $this->db->fetch('alabama/montgomery');
+		} catch(Exception $e){
+			$caught = true;
+		}
+		
+		$this->assertTrue($caught);			
+	}
+	
+	public function test_malformed_json_throws_exception_on_fetch(){
+		$filepath = __DIR__ . '/badJson.json';
+		$this->db = new FRMS\Db\Json($filepath);
+	
+		$caught = false;
+		try {
+			$value = $this->db->fetch('alabama/montgomery');
+		} catch(Exception $e){
+			$caught = true;
+		}
+	
+		$this->assertTrue($caught);
 	}
 	
 	
