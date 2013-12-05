45c0e4025dbc223b90a84127f902b9b64893b2c6
unknown <Kyle.Almas@DFRFRM025686.de-ent.com>
2013-12-05 10:59:47 -0500

    moves bootstraping into bootstrap.php

--- web/index.php
+++ web/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . '/../bootstrap.php';
 
 $app = new Silex\Application();
 $app->register(new Igorw\Silex\ConfigServiceProvider(__DIR__ . '/../config.json'));
